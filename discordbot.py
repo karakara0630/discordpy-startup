a86b3e8fd376e16c4eec67dcd42ac7bb1d51dbd7
karakara0630 <69411098+karakara0630@users.noreply.github.com>
2020-08-11 11:24:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,65 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def hi(ctx):
+    await ctx.send('はぁい！')
 
 
 bot.run(token)
+// Response for Uptime Robot
+const http = require('http');
+http.createServer(function(request, response)
+{
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Discord bot is active now \n');
+}).listen(3000);
+
+// Discord bot implements
+
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.on('ready', message =>
+{
+	console.log('bot is ready!');
+});
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  let newUserChannel = newMember.voiceChannel
+  let oldUserChannel = oldMember.voiceChannel
+
+
+  if(oldUserChannel === undefined && newUserChannel !== undefined) {
+    let channelID;
+    let channels = newMember.guild.channels;
+    channelLoop:
+    for (let c of channels) {
+      let channelType = c[1].type;
+      if (channelType === "text") {
+        channelID = c[0];
+        break channelLoop;
+      }
+    }
+    client.channels.get(channelID).send(newMember.user.username + " がチャンネル 「" + newUserChannel.name + "」 に入室したよ。");
+  } else if(newUserChannel === undefined){
+    let channelID;
+    let channels = oldMember.guild.channels;
+    channelLoop:
+    for (let c of channels) {
+      let channelType = c[1].type;
+      if (channelType === "text") {
+        channelID = c[0];
+        break channelLoop;
+      }
+    }
+    client.channels.get(channelID).send(newMember.user.username + " がチャンネル 「" + oldUserChannel.name + "」 から退室したよ。");
+  }
+})
+
+if(process.env.DISCORD_BOT_TOKEN == undefined)
+{
+	console.log('please set ENV: DISCORD_BOT_TOKEN');
+	process.exit(0);
+}
+
+client.login( process.env.DISCORD_BOT_TOKEN );
